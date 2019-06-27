68a752226c1ffbc89e03f2a7a7025306a224ec6b
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-06-26 21:26:59 -0400

    chore: make eject command

--- .github/scripts/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/iamogbz/node-js-boilerplate ../$REPO_NAME
-cd ../$REPO_NAME
-git remote set-url origin $REPO_URL
-sed -i '' 's/oss-boilerplate/node-js-boilerplate/g' Makefile
-make upstream
